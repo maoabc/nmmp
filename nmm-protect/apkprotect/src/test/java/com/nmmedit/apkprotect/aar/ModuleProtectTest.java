7555b334c6346617b4e9bb491d647072b2f6c049
mao <maokai1905@gmail.com>
2023-01-17 20:30:16 +0800

    修复错误测试代码

--- nmm-protect/apkprotect/src/test/java/com/nmmedit/apkprotect/aar/ModuleProtectTest.java
+++ nmm-protect/apkprotect/src/test/java/com/nmmedit/apkprotect/aar/ModuleProtectTest.java
@@ -1,14 +1,13 @@
 package com.nmmedit.apkprotect.aar;
 
 import com.nmmedit.apkprotect.util.FileUtils;
-import junit.framework.TestCase;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class ModuleProtectTest extends TestCase {
+public class ModuleProtectTest {
 
     private File getTestFile() throws IOException {
         final File tmp = File.createTempFile("tmp", "testAar.aar");
